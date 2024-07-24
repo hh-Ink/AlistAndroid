715b5ddc872a5d65c3d55501d84950aea754baa3
Ink <xuzhen@corpvip.com>
2024-07-24 14:44:37 +0800

    fix: 3.36.0 编译失败

--- alist-lib/scripts/install_aar.sh
+++ alist-lib/scripts/install_aar.sh
@@ -4,5 +4,8 @@ else
   gomobile bind -ldflags "-s -w" -v -target="android/arm,android/arm64" -androidapi 21 "github.com/alist-org/alist/v3/alistlib"
 fi
 
+
+ls -al
+
 mkdir -p ../../app/libs
 cp -f *.aar ../../app/libs
