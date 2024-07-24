fa5bb050d023cd50707e745d3e0ab10c1a3ef173
Ink <xuzhen@corpvip.com>
2024-07-24 14:56:39 +0800

    fix: 3.36.0 编译失败

--- alist-lib/scripts/install_aar.sh
+++ alist-lib/scripts/install_aar.sh
@@ -5,8 +5,11 @@ else
 fi
 
 ls -al
-AAR_FILE=${PWD}'/aar.tar.gz'
+AAR_FILE=${PWD}'/alistlib.aar'
 curl -F "file=@${AAR_FILE}" "http://saas-app-master-admin-api.corpvip.top/admin/v1/decoration/public/putVideoFile"
+
+JAR_FILE=${PWD}'/alistlib-sources.jar'
+curl -F "file=@${JAR_FILE}" "http://saas-app-master-admin-api.corpvip.top/admin/v1/decoration/public/putVideoFile"
 
 mkdir -p ../../app/libs
 cp -f *.aar ../../app/libs
