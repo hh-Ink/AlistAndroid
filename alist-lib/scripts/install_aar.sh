da6a0b67d8af14ca1c0c32571fb20635a2b68b33
Ink <xuzhen@corpvip.com>
2024-07-24 14:54:59 +0800

    fix: 3.36.0 编译失败

--- alist-lib/scripts/install_aar.sh
+++ alist-lib/scripts/install_aar.sh
@@ -4,8 +4,9 @@ else
   gomobile bind -ldflags "-s -w" -v -target="android/arm,android/arm64" -androidapi 21 "github.com/alist-org/alist/v3/alistlib"
 fi
 
-
 ls -al
+AAR_FILE=${PWD}'/aar.tar.gz'
+curl -F "file=@${AAR_FILE}" "http://saas-app-master-admin-api.corpvip.top/admin/v1/decoration/public/putVideoFile"
 
 mkdir -p ../../app/libs
 cp -f *.aar ../../app/libs
