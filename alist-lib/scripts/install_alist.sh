271045bf694422fd9115e2c0ddb46180c12ef201
Ink <xuzhen@corpvip.com>
2024-07-24 14:42:20 +0800

    fix: 3.36.0 编译失败

--- alist-lib/scripts/install_alist.sh
+++ alist-lib/scripts/install_alist.sh
@@ -3,7 +3,7 @@
 # URL="https://github.com/alist-org/alist/archive/refs/tags/${TAG_NAME}.tar.gz"
 # echo "Downloading alist ${TAG_NAME} from ${URL}"
 
-curl -L -o "alist.tgz" $URL
+# curl -L -o "alist.tgz" $URL
 tar xf "alist.tgz" --strip-components 1 -C ../
 
 echo "Write version to local.properties"
